c818cc98b45ca69ee221720ab2c86ea163f08b68
“Piero793” <pierolacitignola7@gmail.com>
2025-01-24 14:55:52 +0100

    arrays

---
+++ assets/script/backOffice.js
@@ -0,0 +1,81 @@
+const productData = [
+  {
+    name: "Cuffie Wireless",
+    description: "Esperienza audio di livello superiore",
+    brand: "Audio bla",
+    imageUrl:
+      "https://images.unsplash.com/photo-1606220945770-b5b6c2c55bf1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y3VmZmllfGVufDB8fDB8fHww",
+    price: 100,
+  },
+  {
+    name: "Chitarra Elettrica",
+    description: "Lo strumento perfetto",
+    brand: "Guitars Hero",
+    imageUrl:
+      "https://images.unsplash.com/photo-1516924962500-2b4b3b99ea02?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaXRhcnJhfGVufDB8fDB8fHww",
+    price: 599,
+  },
+
+  {
+    name: "WebCam",
+    description: "Massima risoluzione e microfono incorporato",
+    brand: "Security WebCam",
+    imageUrl:
+      "https://images.unsplash.com/photo-1623949556303-b0d17d198863?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2ViY2FtfGVufDB8fDB8fHww",
+    price: 120,
+  },
+
+  {
+    name: "SunGlasses",
+    description: "Massima protezione contro i raggi UV",
+    brand: "OKLEY",
+    imageUrl:
+      "https://images.unsplash.com/photo-1502767089025-6572583495f9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHN1bmdsYXNzZXN8ZW58MHx8MHx8fDA%3D",
+    price: 150,
+  },
+
+  {
+    name: "Berretto VA-RVCA",
+    description: "Stile Perfetto",
+    brand: "VA-RVCA",
+    imageUrl:
+      "https://images.unsplash.com/photo-1556306535-0f09a537f0a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhhdHxlbnwwfHwwfHx8MA%3D%3D",
+    price: 40,
+  },
+
+  {
+    name: "Canon RF-400",
+    description: "Camera Canon 2025",
+    brand: "Canon",
+    imageUrl:
+      "https://images.unsplash.com/photo-1533425962554-06f6d8c4dacc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fub258ZW58MHx8MHx8fDA%3D",
+    price: 650,
+  },
+
+  {
+    name: "Body Cream",
+    description: "Cream for body daily routine",
+    brand: "Neauthy",
+    imageUrl:
+      "https://images.unsplash.com/photo-1601049413574-214d105b26e4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNyZWFtfGVufDB8fDB8fHww",
+    price: 25,
+  },
+
+  {
+    name: "Kettlebell 9kg",
+    description: "Equipment for body training",
+    brand: "New gym",
+    imageUrl:
+      "https://images.unsplash.com/photo-1632077804406-188472f1a810?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2V0dGxlYmVsbHxlbnwwfHwwfHx8MA%3D%3D",
+    price: 45,
+  },
+
+  {
+    name: "Lampada da scrivania",
+    description: "Pratica e regolabile in tutte le direzioni",
+    brand: "Nine Light",
+    imageUrl:
+      "https://images.unsplash.com/photo-1504194008492-c55ffe34e18d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGFtcGFkYXxlbnwwfHwwfHx8MA%3D%3D",
+    price: 18,
+  },
+];
